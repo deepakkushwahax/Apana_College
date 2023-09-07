72691e315d1262718c6495938161d94fa134f68f
deepakkushwahax <deepakkushwaha7523@gmail.com>
2023-09-07 23:37:48 +0530

    init

--- src/Simple_Interest.java
+++ src/Simple_Interest.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class Simple_Interest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float P = sc.nextFloat();
+        float R = sc.nextFloat();
+        float T = sc.nextFloat();;
+        float Interest = (P*R*T)/100;
+        System.out.println(Interest);
+    }
+
 
 }
