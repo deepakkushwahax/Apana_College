5bf4766ebcf3a9cb9f791e6589c84a93b193c240
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2023-09-11 22:07:43 +0530

    Add files via upload

---
+++ Type_promotion.java
@@ -0,0 +1,18 @@
+public class Type_promotion {
+    // Type Promotion - Type Promotion is a method that convert the small data type into Large Data Types. and type promotion only happened in the Expression.
+    public static void main(String[] args) {
+//        char a = 'a';
+//        char b = 'b';
+//        System.out.println((int)(b));
+//        System.out.println((int)(a));
+//        System.out.println(b-a);
+//        System.out.println(a);
+//
+        int a  = 10;
+        float b = 20.20f;
+        long c = 30;
+        double d = 25;
+        double ans = a + b + c + d;
+        System.out.println(ans);
+    }
+}
