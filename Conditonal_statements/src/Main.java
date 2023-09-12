ffe4483f1178705ca749efc3938fcecee3295f50
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2023-09-12 23:57:08 +0530

    Add files via upload

---
+++ Conditonal_statements/src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int age = 26;
+        if (age >= 18){
+            System.out.println("adult : drive, vote");
+        }
+        if (age > 13 && age < 18){
+            System.out.println("teenager");
+        }
+        else {
+            System.out.println("not adult");
+        }
+
+
+
+ }
+}
---
+++ Conditonal_statements/src/Odd_Even.java
@@ -0,0 +1,5 @@
+public class Odd_Even {
+    public static void main(String[] args) {
+        
+    }
+}
