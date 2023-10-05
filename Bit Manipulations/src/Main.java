7729c2c5196cd58c746d6e93f772b65101b2b4dc
deepakkushwahax <deepakkushwaha7523@gmail.com>
2023-10-06 00:57:23 +0530

    added the given number is EvenOROdd

--- Bit Manipulations/src/Main.java
+++ Bit Manipulations/src/Main.java
@@ -1,6 +1,18 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    public static void oddEven(int n){
+         int bitMask = 1;
+         if ((n & bitMask) == 0){
+             //even number
+            System.out.println("Even number");
+        } else {
+            System.out.println("odd number");
+        }
+
+        }
+
     public static void main(String[] args) {
         // AND Bitwise Operator
         System.out.println(5&6);
@@ -19,5 +31,11 @@ public class Main {
 
         // Right shift Bitwise Operator
         System.out.println(5>>6);
+
+        // find given number is Even or Odd
+        oddEven(2);
+        oddEven(3);
+
+
     }
 }
