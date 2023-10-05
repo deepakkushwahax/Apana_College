88dd72584cdb71c75301acd33395515ac8d68dcb
deepakkushwahax <deepakkushwaha7523@gmail.com>
2023-10-05 22:31:07 +0530

    Added Bit Manipulations

---
+++ Bit Manipulations/src/Main.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // AND Bitwise Operator
+        System.out.println(5&6);
+
+        // OR Bitwise Operator
+        System.out.println(5|6);
+
+        // OR Bitwise Operator
+        System.out.println(5^6);
+
+        // 1's  Bitwise Operator
+        System.out.println(~1);
+
+        // Left shift Bitwise Operator
+        System.out.println(5<<6);
+
+        // Right shift Bitwise Operator
+        System.out.println(5>>6);
+    }
+}
