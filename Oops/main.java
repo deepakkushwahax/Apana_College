b7a0d6e5848e7444a65ab1a35f8be0cce97b8b1e
deepakkushwahax <deepakkushwaha7523@gmail.com>
2023-10-06 01:30:37 +0530

    Opps

---
+++ Oops/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void Pattern(int n){
+        int num;
+        for (int i=1; i<num; i++){
+            for (int j=0; j>num-1-i; j--);
+        }
+        System.out.println("*");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Pattern(5););
+
+    }
+}
