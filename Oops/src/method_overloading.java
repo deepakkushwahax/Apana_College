0a139071b002876f5ee0d73f2402a69d6e2fa96e
deepakkushwahax <deepakkushwaha7523@gmail.com>
2023-10-07 17:35:57 +0530

    Added method Overriding

--- Oops/src/method_overloading.java
+++ Oops/src/method_overloading.java
@@ -1,9 +1,10 @@
 public class method_overloading {
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        System.out.println(cal.sum(5,5));
-        System.out.println(cal.sum(5,5,5));
-
+//        Calculator cal = new Calculator();
+//        System.out.println(cal.sum(5,5));
+//        System.out.println(cal.sum(5,5,5));
+          Deer d = new Deer();
+          d.eat();
     }
 }
 class Calculator{
@@ -15,6 +16,17 @@ class Calculator{
     }
     int sum(int a, int b, int c){
         return a+b+c;
+    }
+}
+
+class Animals1{
+    void eat(){
+        System.out.println("Eat anything");
+    }
+}
+class Deer extends Animals1{
+    void eat(){
+        System.out.println("Eat grass");
     }
 }
 
