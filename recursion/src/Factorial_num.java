16a09731042ddf642623be9c8df670c259da0dcb
deepakkushwahax <deepakkushwaha7523@gmail.com>
2023-10-23 06:49:41 +0530

    added factorial num

---
+++ recursion/src/Factorial_num.java
@@ -0,0 +1,16 @@
+public class Factorial_num {
+    public static int fact(int n){
+        if (n == 0){
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n*fact(n-1);
+        return fn;
+
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fact(5));
+
+    }
+}
